0dd4b3183bacdcf2fa01e42239238b51c8b122c6
saramnim <12hyeji24@gmail.com>
2023-06-09 22:59:31 +0900

    create: origin files

---
+++ client/src/components/Register/Register.jsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container } from './style';
+
+const Register = () => {
+  return <Container></Container>;
+};
+export default Register;
---
+++ client/src/components/Register/style.js
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import '../../GlobalStyle';
+
+export const Container = styled.div``;
