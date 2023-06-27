91c864be752f3186115118768fe7178af23fc5cd
tyoung <david3241@naver.com>
2023-06-27 18:07:14 +0900

    feat:homeRep

--- client/src/styles/homestyle.js
+++ client/src/styles/homestyle.js
@@ -21,71 +21,182 @@ export const Title = styled.h1`
   font-size: 70px;
   margin-top: 25vh;
   color: #fff;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 65px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 60px;
+    margin-top: 5vh;
+  }
 `;
 
 export const H1 = styled.h1`
   font-size: 18px;
   text-align: center;
   color: #fff;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 17px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 16px;
+    margin-top: -5vh;
+  }
 `;
 
 export const Music = styled.div`
   background-image: url('/assets/image1.png');
   background-size: cover;
-  margin-top: 30%;
   width: 100px;
   height: 100px;
+
+  @media only screen and (max-width: 1024px) {
+    width: 90px;
+    height: 90px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 export const PlayList = styled.div`
   background-image: url('/assets/image2.png');
   background-size: cover;
-  margin-top: 30%;
-  width: 100px;
-  height: 100px;
+  width: 95px;
+  height: 95px;
+
+  @media only screen and (max-width: 1024px) {
+    width: 90px;
+    height: 90px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 export const Chat = styled.div`
   background-image: url('/assets/image3.png');
   background-size: cover;
-  margin-top: 30%;
   width: 100px;
   height: 100px;
+
+  @media only screen and (max-width: 1024px) {
+    width: 90px;
+    height: 90px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `;
 
 export const WhiteBox = styled.div`
   width: 100%;
   height: 40vh;
   background-color: white;
-  position: fixed;
   bottom: 0;
-  v-index: -1;
+  z-index: -2;
+  overflow-y: auto;
+  position: fixed;
+
+  @media only screen and (max-width: 768px) {
+    top: 40vh;
+    height: 60vh;
+    position: absolute;
+  }
 `;
 
 export const ImageWithTextContainer = styled.div`
   display: flex;
   align-items: center;
-  height: 90%;
+  height: 100%;
   justify-content: space-evenly;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    width: 100%;
+  }
 `;
 
 export const ImageWrapper = styled.div`
-  height: 200px;
+  display: flex;
+  height: 100px;
   background-size: cover;
   align-items: center;
   text-align: center;
+  flex-direction: column;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: row;
+    width: 100%;
+  }
 `;
 
 export const Text = styled.button`
   font-family: 'HSJiptokki-Round';
-  border-radius: 5px;
   background-color: transparent;
   color: #ff6060;
   font-weight: bold;
   font-size: 20px;
   margin-top: 10px;
+  text-decoration: none;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 17px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 20px;
+  }
 `;
 
-export const activeStyle = {
-  color: '#fff',
-};
+export const Text1 = styled.button`
+  font-family: 'HSJiptokki-Round';
+  background-color: transparent;
+  color: #ff6060;
+  font-weight: bold;
+  font-size: 20px;
+  margin-top: 10px;
+  text-decoration: none;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: 17px;
+  }
+
+  @media only screen and (max-width: 768px) {
+    font-size: 20px;
+    color: #fff;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: row;
+    width: 100%;
+    // hight: 100px;
+    padding-left: 15vh;
+    justify-content: space-between;
+  }
+`;
+
+export const Middle = styled.div`
+  display: flex;
+
+  @media only screen and (max-width: 768px) {
+    hight: 130px;
+    width: 100%;
+    background-color: #ff6060;
+  }
+`;
